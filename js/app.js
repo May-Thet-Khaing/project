256ee6b9608da0e3acf67bfad4c75ac4aa7eb014
May Thet Khaing <maythetkhaing317@gmail.com>
2022-07-11 11:02:08 +0630

    fourth project

--- js/app.js
+++ js/app.js
@@ -1,42 +1,35 @@
-const days=document.getElementById('days');
-const hours=document.getElementById('hours');
-const minutes=document.getElementById('minutes');
-const seconds=document.getElementById('seconds');
-const milliseconds=document.getElementById('milliseconds');
+const sendbtn=document.getElementById('send');
+const panel=document.getElementById('panel');
+const ratingcontainer=document.querySelector('.ratings-container');
+const ratings = document.querySelectorAll('.rating');
 
+let selectrating='satisfied';
+// console.log(selectrating);
 
-// console.log(newyeardate);
+ratingcontainer.addEventListener('click',(e)=>{
 
-function updatetime(){
-    const date=new Date();
-    const currentyear=new Date().getFullYear();
-    const newyeardate=new Date(`January 01 ${currentyear +1} 00:00:00`);
+    if(e.target.classList.contains('rating')){
+        removeactive();
+        e.target.classList.add('active');
+        selectrating=e.target.lastElementChild.textContent;
+    }else if(e.target.parentNode.classList.contains("rating")){
+        removeactive();
+        e.target.parentNode.classList.add('active');
+        selectrating = e.target.nextElementSibling.textContent;
 
-    const diff=newyeardate-date;
-    // console.log(diff);
+    }
+});
 
-    const day=Math.floor(diff/1000/60/60/24);
-    // console.log(day);
-
-    const hour=Math.floor(diff/1000/60/60)  % 24;
-    // console.log(hour);
-    
-    const minute=Math.floor(diff/1000/60) % 60;
-    // console.log(minute);
-
-    const second=Math.floor(diff/1000) % 60;
-    // console.log(second);
-
-    const millisecond=Math.floor(diff) % 100;
-    // console.log(millisecond);
-
-    days.innerText=day;
-    hours.innerText=hour > 10 ? hour : "0" + hour;
-    minutes.innerText=minute >10 ? minute : "0" + minute;
-    seconds.innerText=second >10 ? second : "0" + second; 
-
-
+function removeactive(){
+  ratings.forEach(rating=>{
+        rating.classList.remove('active');
+  })
 }
-setInterval(updatetime,1000);
 
+sendbtn.addEventListener('click',()=>{
+    panel.innerHTML=`
+    <strong>Thank you for your feedbacks</strong>
+    <p>${selectrating}</p>
+    `;
+})
 
